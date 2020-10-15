b8eed4f523ba21f1d033b4dd032466db3deec7ad
Angela Mitchell <anglarenmit@gmail.com>
2020-10-15 11:04:50 -0500

    Completed bonus missions

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
 window.addEventListener("load", function() {
 
     let takeoff = document.getElementById("takeoff");
@@ -9,8 +10,22 @@ window.addEventListener("load", function() {
     let flightStatus = document.getElementById("flightStatus");
     let shuttleBackground = document.getElementById("shuttleBackground");
     let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    let spaceShuttleWidth = document.getElementById("spaceShuttleWidth");
 
-    // let rocket = document.getElementById("rocket");
+    let rocket = document.getElementById("rocket");
+
+    // Ready for click commands
+    rocket.style.left = '0px';
+    rocket.style.bottom = '0px';
+
+    // Moves image to bottom left corner
+    rocket.style.position = 'absolute';
+
+    // Use these variables if you set ID's for buttons
+    let up = document.getElementById("up");
+    let down = document.getElementById("down");
+    let right = document.getElementById("right");
+    let left = document.getElementById("left");
 
     takeoff.addEventListener("click", function() {
         let response = window.confirm("Are you sure the shuttle is ready for takeoff?");
@@ -26,7 +41,7 @@ window.addEventListener("load", function() {
         flightStatus.innerHTML = 'The shuttle has landed.';
         shuttleBackground.style.background = 'green';
         spaceShuttleHeight.innerHTML = '0';
-        // rocket.style.bottom = 0;
+        rocket.style.bottom = 0;
     });
 
     missionAbort.addEventListener("click", function() {
@@ -35,32 +50,76 @@ window.addEventListener("load", function() {
             flightStatus.innerHTML = 'Mission aborted.';
             shuttleBackground.style.background = 'green';
             spaceShuttleHeight.innerHTML = '0';  
-            // rocket.style.bottom = 0;  
+            rocket.style.bottom = 0;  
+        }
+    });
+
+    // Place listeners for ID approach inside window listener
+
+    up.addEventListener("click", function() {
+        if (spaceShuttleHeight.innerHTML < 250000) {
+            let movement = parseInt(rocket.style.bottom) + 10 + 'px';
+            rocket.style.bottom = movement;
+            spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) + 10000;
+        }
+    });
+
+    down.addEventListener("click", function() {
+        if (spaceShuttleHeight.innerHTML > 10000) {
+            let movement = parseInt(rocket.style.bottom) - 10 + 'px';
+            rocket.style.bottom = movement;
+            spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) - 10000;
+        }
+    });
+
+    right.addEventListener("click", function() {
+        if (spaceShuttleWidth.innerHTML < 550000) {
+            let movement = parseInt(rocket.style.left) + 10 + 'px';
+            rocket.style.left = movement;
+            spaceShuttleWidth.innerHTML = Number(spaceShuttleWidth.innerHTML) + 10000;
+        }
+    });
+
+    left.addEventListener("click", function() {
+        if (spaceShuttleWidth.innerHTML > -20000) {
+            let movement = parseInt(rocket.style.left) - 10 + 'px';
+            rocket.style.left = movement;
+            spaceShuttleWidth.innerHTML = Number(spaceShuttleWidth.innerHTML) - 10000;
         }
     });
 
 });
 
-// let rocket = document.getElementById("rocket");
-
-// function down() {
-//     let movement = parseInt(rocket.style.bottom) - 10 + 'px';
-//     rocket.style.bottom = movement;
-//     spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) - 10000; 
-// }
+// Place outside of window listener if you do onclick approach
 
 // function up() {
-//     let movement = parseInt(rocket.style.bottom) + 10 + 'px';
-//     rocket.style.bottom = movement;
-//     spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) + 10000; 
-// }
+//     if (spaceShuttleHeight.innerHTML < 250000) {
+//         let movement = parseInt(rocket.style.bottom) + 10 + 'px';
+//         rocket.style.bottom = movement;
+//         spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) + 10000;
+//     }
+// };
+
+// function down() {
+//     if (spaceShuttleHeight.innerHTML > 10000) {
+//         let movement = parseInt(rocket.style.bottom) - 10 + 'px';
+//         rocket.style.bottom = movement;
+//         spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) - 10000;
+//     }
+// };
 
 // function right() {
-//     let movement = parseInt(rocket.style.left) + 10 + 'px';
-//     rocket.style.left = movement;
-// }
+//     if (spaceShuttleWidth.innerHTML < 550000) {
+//         let movement = parseInt(rocket.style.left) + 10 + 'px';
+//         rocket.style.left = movement;
+//         spaceShuttleWidth.innerHTML = Number(spaceShuttleWidth.innerHTML) + 10000;
+//     }
+// };
 
 // function left() {
-//     let movement = parseInt(rocket.style.left) - 10 + 'px';
-//     rocket.style.left = movement;
-// }
+//     if (spaceShuttleWidth.innerHTML > -20000) {
+//         let movement = parseInt(rocket.style.left) - 10 + 'px';
+//         rocket.style.left = movement;
+//         spaceShuttleWidth.innerHTML = Number(spaceShuttleWidth.innerHTML) - 10000;
+//     }
+// };
